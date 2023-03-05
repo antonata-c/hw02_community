101de8a22d212e10da032c32ca571a818a629859
Михаил Селиверстов <seliverstov97@gmail.com>
2023-03-05 10:14:15 +0300

    Исправлены недочеты

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-# Create your views here.
+
+COUNT_POSTS: int = 10
 
 
 def index(request):
     template = 'posts/index.html'
     text = 'Это главная страница проекта Yatube'
     title = 'Последние обновления на сайте'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:COUNT_POSTS]
     # В словаре context отправляем информацию в шаблон
     context = {
         'title': title,
@@ -19,12 +20,12 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    group = get_object_or_404(Group, slug=slug)
     text = 'Лев Толстой – зеркало русской революции.'
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts_set.all()[:COUNT_POSTS]
     context = {
         'text': text,
-        'group': group,
+        'groupч': group,
         'posts': posts,
     }
 
